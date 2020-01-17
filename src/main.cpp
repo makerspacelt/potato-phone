ef469f5a812b1dab3b36be2ce754949c12b0a494
Your Name <you@example.com>
2020-01-17 22:49:37 +0000

    adding platformio setup

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+/*
+  Rotary phone number counter using interrupts
+  More info to come later
+*/
+
+const int dialpadPin = 2;
+const int numberStopPin = 3;
+String phoneNumber = "";
+int num = 0;
+
+void pulseCounter() {
+  num += 1;
+}
+
+void endStop() {
+  phoneNumber += num == 10 ? 0 : num;
+  Serial.println(phoneNumber);
+  num = 0;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(dialpadPin, INPUT);
+  pinMode(numberStopPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(dialpadPin), pulseCounter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(numberStopPin), endStop, FALLING);
+}
+
+void loop() {
+  // skip
+}
