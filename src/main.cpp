501ba524197f090a4bd22d66433fb5ccad0e2822
Renardas <kulverstukas.k@gmail.com>
2020-01-18 21:19:43 +0200

    development in progress

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,41 @@
   More info to come later
 */
 
-const int dialpadPin = 2;
-const int numberStopPin = 3;
+//--------
+const int DIALPAD_PIN = 3;
+const int NUMBER_STOP_PIN = 2;
+const int PHONE_PICKED_UP_PIN = 4;
+//--------
 String phoneNumber = "";
-int num = 0;
+int dialedNum = 0;
+boolean phonePickedUp = false;
+//--------
 
 void pulseCounter() {
-  num += 1;
+  dialedNum += 1;
 }
 
-void endStop() {
-  phoneNumber += num == 10 ? 0 : num;
+void numStop() {
+  phoneNumber += (dialedNum == 10) ? 0 : dialedNum;
   Serial.println(phoneNumber);
-  num = 0;
+  dialedNum = 0;
+}
+
+void pickupPhone() {
+  phonePickedUp = (digitalRead(PHONE_PICKED_UP_PIN) == LOW); // phone was picked up, switch is ON
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(dialpadPin, INPUT);
-  pinMode(numberStopPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(dialpadPin), pulseCounter, FALLING);
-  attachInterrupt(digitalPinToInterrupt(numberStopPin), endStop, FALLING);
+  pinMode(DIALPAD_PIN, INPUT);
+  pinMode(NUMBER_STOP_PIN, INPUT);
+  pinMode(PHONE_PICKED_UP_PIN, INPUT);
+  digitalWrite(PHONE_PICKED_UP_PIN, HIGH); // enable internal pullup
+  attachInterrupt(digitalPinToInterrupt(DIALPAD_PIN), pulseCounter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(NUMBER_STOP_PIN), numStop, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PHONE_PICKED_UP_PIN), pickupPhone, CHANGE);
 }
 
 void loop() {
-  // skip
+  
 }
