4e49faa834f63ae2210ec92d0eb506a330befd42
Šarūnas Stašaitis <sarustasaitis@gmail.com>
2021-12-04 00:27:59 +0200

    Eina paskambint iš arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <AltSoftSerial.h>
+#include <InputDebounce.h>
 
 /*
   Rotary phone number counter using interrupts
@@ -12,6 +13,15 @@ const int NUMBER_STOP_PIN = 2; // Blue wire
 const int PHONE_PICKED_UP_PIN = 4;
 const int NUMBER_TIMEOUT_MS = 4000;
 const int RING_TIMEOUT_MS = 6000;
+
+const int DIAL_DEBOUNCE_DELAY = 20;
+const int BUTTON_DEBOUNCE_DELAY = 10;
+//-------------
+
+static InputDebounce dialEnable;
+static InputDebounce dialPulse;
+static InputDebounce buttonCall;
+
 //-------------
 String phoneNumber = "";
 int pulses = 0;
@@ -55,23 +65,25 @@ void flushSerialBuffer() {
 }
 
 void pulseCounter() {
+  Serial.println("Pulse detected " + String(millis()));
   if (phonePickedUp && !isOnCall) {
     pulses += 1;
   }
 }
 
-void numStop() {
-  if (phonePickedUp && !isOnCall) {
+void endDialPulseCount(uint8_t pinIn) {
+  // Serial.println("Num stop called");
+  // if (phonePickedUp && !isOnCall) {
     if (isDialing) {
       millisSinceLastNumStop = 0;
     } else {
       isDialing = true;
     }
     phoneNumber += (pulses == 10) ? 0 : pulses;
-    Serial.println(phoneNumber);
+    Serial.println("Phone number: " + phoneNumber);
     pulses = 0;
     millisSinceLastNumStop = millis();
-  }
+  // }
 }
 
 void updateSerial() {
@@ -93,6 +105,9 @@ void answerCall() {
 }
 
 void pickupPhone() {
+  if(!phonePickedUp) {
+    Serial.println("Pickup phone");
+  }
   phonePickedUp = true;
   if (isCalling) {
     answerCall();
@@ -100,6 +115,9 @@ void pickupPhone() {
 }
 
 void hungupPhone() {
+  if (phonePickedUp) {
+    Serial.println("Hungup phone");
+  }
   phonePickedUp = false;
   isDialing = false;
   isCalling = false;
@@ -115,36 +133,84 @@ void hungupPhone() {
 
 void callNumber() {
   String telNumCommand = "ATD"+phoneNumber+";";
-  telNumCommand = "ATD860546993;";
-  gsm.print(telNumCommand);
-  String data = Serial.readString();
-  if (data.indexOf("OK") == -1) {
-    Serial.println("ATH"); // something wrong, best to hung up here
+  // telNumCommand = "ATD867961606;";
+  Serial.println("Calling: " + telNumCommand);
+  gsm.println(telNumCommand);
+  // String data = Serial.readString();
+  // if (data.indexOf("OK") == -1) {
+  //   Serial.println("ATH"); // something wrong, best to hung up here
+  // }
+}
+
+void startDialPulseCount(uint8_t pinIn) {
+  Serial.println("Start pulse count");
+  pulses = 0;
+}
+
+void pulseDial(int8_t pinIn) {
+  Serial.println("Pulse dial");
+  pulses++;
+}
+
+void call(int8_t pinIn) {
+  if(phoneNumber.length() != 10) {
+    callNumber();
+  } else {
+    Serial.println("Number cleared");
   }
+  phoneNumber = "";
 }
 
 void setup() {
   Serial.begin(9600);
   gsm.begin(9600);
   // delay(500);
-  gsm.println("AT");
-  updateSerial();
+  // gsm.println("AT");
+  // updateSerial();
   // flushSerialBuffer();
-  callNumber();
+  // callNumber();
 
   pinMode(DIALPAD_PIN, INPUT);
   pinMode(NUMBER_STOP_PIN, INPUT);
   pinMode(PHONE_PICKED_UP_PIN, INPUT);
+  pinMode(12, INPUT);
+
   digitalWrite(PHONE_PICKED_UP_PIN, HIGH);
-  attachInterrupt(digitalPinToInterrupt(DIALPAD_PIN), pulseCounter, FALLING);
-  attachInterrupt(digitalPinToInterrupt(NUMBER_STOP_PIN), numStop, FALLING);
+
+  dialEnable.registerCallbacks(NULL, endDialPulseCount);
+  dialPulse.registerCallbacks(pulseDial, NULL);
+  buttonCall.registerCallbacks(call, NULL);
+
+  dialEnable.setup(NUMBER_STOP_PIN, DIAL_DEBOUNCE_DELAY, InputDebounce::PIM_EXT_PULL_UP_RES, NULL, InputDebounce::ST_NORMALLY_CLOSED);
+  dialPulse.setup(DIALPAD_PIN, DIAL_DEBOUNCE_DELAY, InputDebounce::PIM_EXT_PULL_UP_RES);
+  buttonCall.setup(12, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_EXT_PULL_UP_RES, NULL, InputDebounce::ST_NORMALLY_CLOSED);
+
+  // attachInterrupt(digitalPinToInterrupt(12), buttonPushed, RISING);
+  // attachInterrupt(digitalPinToInterrupt(DIALPAD_PIN), pulseCounter, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(NUMBER_STOP_PIN), numStop, FALLING); 
 }
 
+int tmp1 = 0;
+
 void loop() {
-  int w = gsm.println("derp");
-  Serial.print("Written: ");
-  Serial.println(w);
-  delay(500);
+  long now = millis();
+
+  dialEnable.process(now);
+  dialPulse.process(now);
+  buttonCall.process(now);
+
+  // int w = gsm.println("derp");
+  // Serial.print("Written: ");
+  // Serial.println(w);
+  // delay(500);
+  updateSerial();
+  if(tmp1++ % 10 == 0) {
+    int bs = digitalRead(12);
+    String bss = "BS->" + String(bs);
+    bss = bss + "<-BS";
+    // Serial.println(bss);
+  }
+  delay(5);
   return;
   if (gsm.available()) {
     String data = gsm.readString();
